843b87123466bdcc8377e86204fe3b3db9f21c55
Ryan McCauley <ryanmccauley211@gmail.com>
2018-07-23 15:05:12 +0100

    add test for reading from 'start' to 'end' positions

--- csv_reader_test.go
+++ csv_reader_test.go
@@ -1,4 +1,4 @@
-package csv_simple
+package simple_csv
 
 import "testing"
 
@@ -8,11 +8,23 @@ func TestDelimiter(t *testing.T) {
 		t.Errorf("unable to create csvReader: %v", err)
 	}
 
-	reader.SetDelimiter('\t')
+	reader.Delimiter('\t')
 	if err = reader.Read(); err != nil {
 		t.Errorf("unable to read csv file: %v", err)
 	}
 	expectedLines := 210
+	if len(reader.Data) != expectedLines {
+		t.Errorf("expected lines: %d, found: %d", expectedLines, len(reader.Data))
+	}
+}
+
+func TestReadStartEnd(t *testing.T) {
+	reader, _ := NewCSVReader("simple.csv")
+	reader.Start = 2
+	reader.End = 7
+
+	reader.Read()
+	expectedLines := reader.End - reader.Start
 	if len(reader.Data) != expectedLines {
 		t.Errorf("expected lines: %d, found: %d", expectedLines, len(reader.Data))
 	}
